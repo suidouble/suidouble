7e77dbc930701d47d84003a1d483fccb8ac4c7d8
jeka <jeka911@gmail.com>
2023-05-31 18:24:57 +0300

    move out cli logic to wrapper

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const SuiMaster = require('./lib/SuiMaster.js');
 const SuiInBrowser = require('./lib/SuiInBrowser.js');
+const SuiTestScenario = require('./lib/SuiTestScenario.js');
 
 module.exports = {
     SuiMaster,
     SuiInBrowser,
+    SuiTestScenario,
 };
