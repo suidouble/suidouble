5ed783b9deb54646758843f0cfafbf7f8e58d6e2
jeka <jeka911@gmail.com>
2023-07-31 01:17:20 +0300

    export SuiObject

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const SuiMaster = require('./lib/SuiMaster.js');
 const SuiInBrowser = require('./lib/SuiInBrowser.js');
 const SuiTestScenario = require('./lib/SuiTestScenario.js');
+const SuiObject = require('./lib/SuiObject.js');
 const SuiLocalTestValidator = require('./lib/SuiLocalTestValidator.js');
 
 module.exports = {
     SuiMaster,
+    SuiObject,
     SuiInBrowser,
     SuiTestScenario,
     SuiLocalTestValidator,
